e9c5f273ed935c87b6f46f2b82d511db75645fb8
Benny <benny.le890@gmail.com>
2022-12-01 17:36:15 -0500

    finished 20.5.6

--- photo-port/src/components/Contact/index.js
+++ photo-port/src/components/Contact/index.js
@@ -1,19 +1,39 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
 
     const [formState, setFormState ] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
+    
 
-    function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
-    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!errorMessage) {
+          setFormState({ [e.target.name]: e.target.value });
+          console.log('Form', formState);
+        }
+      };
+
+    const handleChange = (e) => {
+        if (e.target.name === 'email') {
+          const isValid = validateEmail(e.target.value);
+          if (!isValid) {
+            setErrorMessage('Your email is invalid.');
+          } else {
+            setErrorMessage('');
+          }
+        } else {
+          if (!e.target.value.length) {
+            setErrorMessage(`${e.target.name} is required.`);
+          } else {
+            setErrorMessage('');
+          }
+        }
+      };
     // console.log(formState);
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(formState);
-    }
     // JSX
     return (
         <section>
@@ -31,6 +51,11 @@ function ContactForm() {
                     <label htmlFor='message'>Message:</label>
                     <textarea name="message" rows="5" defaultValue={message} onChange={handleChange}/>
                 </div>
+                {errorMessage && (
+                <div>
+                    <p className="error-text">{errorMessage}</p>
+                </div>
+                )}
                 <button type="submit">Submit</button>
             </form>
         </section>
--- photo-port/src/components/Nav/index.js
+++ photo-port/src/components/Nav/index.js
@@ -6,6 +6,8 @@ function Nav(props) {
     categories = [],
     setCurrentCategory,
     currentCategory,
+    contactSelected,
+    setContactSelected
   } = props;
 
   useEffect(() => {
@@ -22,12 +24,17 @@ function Nav(props) {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about">
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
-          <li className="mx-2">
-            <span>Contact</span>
+          <li
+            className={`mx-1 ${
+              currentCategory.name === category.name && !contactSelected && `navActive`
+              }`}
+              key={category.name}
+            >
+            <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
@@ -36,9 +43,9 @@ function Nav(props) {
                 }`}
               key={category.name}
             >
-              <span
-                onClick={() => {
-                  setCurrentCategory(category)
+              <span onClick={() => {
+                setCurrentCategory(category);
+                setContactSelected(false);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
